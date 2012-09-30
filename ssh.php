460eeb4f22ad976eb49c21df59ab9d5d3a2effcf
Abhinandan <abhiin1947@gmail.com>
2012-09-30 20:17:23 +0530

    ssh.php improved part 1

--- ssh.php
+++ ssh.php
@@ -13,11 +13,19 @@ $command = "ls -l";*/
 $ssh_conn = ssh2_connect($domain, 22);
 if(!ssh_conn)
 {
-	die("Failed to establish connection");
+	$response['error'] = 1;
+	$response['errorcode'] = 0;
+	$response['error_msg'] = "Failed to establish connection";
+	echo json_encode($response);
+	exit(0);
 	}
 if(ssh2_auth_password($ssh_conn, $user, $pass) == false)
 {
-	die("Invalid details");
+	$response['error'] = 1;
+	$response['errorcode'] = 1;
+	$response['error_msg'] = "Authentication failed";
+	echo json_encode($response);
+	exit(0);
 }
 //store the values in a session
 $_SESSION['connected'] = 1;
@@ -25,15 +33,45 @@ $_SESSION['connection'] = $ssh_conn;
 
 //execute command
 $ostream = ssh2_exec($ssh_conn ,$command. '\n');
+if($ostream == false)
+{
+	$response['error'] = 1;
+	$response['errorcode'] = 2;
+	$response['error_msg'] = "Execution of command failed";
+	echo json_encode($response);
+	exit(0);
+	}
 //TODO
-stream_set_blocking($ostream, true);
+if(stream_set_blocking($ostream, true)==false)
+{
+	$response['error'] = 1;
+	$response['errorcode'] = 3;
+	$response['error_msg'] = "Stream Error";
+	echo json_encode($response);
+	exit(0);
+	}
 
 //echo contents
-echo stream_get_contents($ostream);
+$output = stream_get_contents($ostream);
+if($output == false)
+{
+	$response['error'] = 1;
+	$response['errorcode'] = 4;
+	$response['error_msg'] = "Stream Error";
+	echo json_encode($response);
+	exit(0);
+	}
+else
+{
+	$response['error'] = 0;
+	$response['command'] = $command;
+	$response['output'] = $output;
+	echo json_encode($response);
+	}
 }
 else
 {
-		
+	
 }
 function changedir($newpath)
 {
